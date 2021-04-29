3bd2e7ebffcdf136e7012f7dcb2222d382d5e007
Juliomdz <julio.mdz@hotmail.com>
2021-04-29 18:33:57 -0300

    en blanco

---
+++ PrimerParcial/Debug/src/PrimerParcial.d
@@ -0,0 +1,3 @@
+src/PrimerParcial.o: ../src/PrimerParcial.c ../src/arr.h ../src/utn.h
+../src/arr.h:
+../src/utn.h:
---
+++ PrimerParcial/src/PrimerParcial.c
@@ -0,0 +1,23 @@
+/*
+ ============================================================================
+ Name        : PrimerParcial.c
+ Author      : Julio Mendez
+ Enunciado   :
+
+
+
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdio_ext.h>
+#include "arr.h"
+#include "utn.h"
+
+int main(void) {
+	puts("hello world"); /* prints  */
+	return EXIT_SUCCESS;
+}
